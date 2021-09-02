97b8338f5c0574929c405f14dbb4b2b88d6bffdf
Maria Kavanagh <mariakavanagh25@gmail.com>
2021-09-02 14:27:48 +0100

    Tidied up into for loop

--- Data_challenge_maria_kavanagh.R
+++ Data_challenge_maria_kavanagh.R
@@ -27,64 +27,22 @@ acc_sales <- rbind(acc_sales, x%>% filter(CUSTOMER_NAME_y !='NA') )
 acc_sales <- rename(acc_sales, CUSTOMER_NAME.x = CUSTOMER_NAME_x)
 acc_sales <- rename(acc_sales, CUSTOMER_NAME.y = CUSTOMER_NAME_y)
 
-### find those records that were not matched and try again (203 records)
-sales_unmatched <- subset(sales, !(CUSTOMER_NAME_y %in% acc_sales$CUSTOMER_NAME.y ))
-account_unmatched <- subset(account, !(CUSTOMER_NAME_x %in% acc_sales$CUSTOMER_NAME.x))
+check <- (nrow(account) - nrow(acc_sales))
 
-account_unmatched <- rename(account_unmatched, CUSTOMER_NAME = CUSTOMER_NAME_x)
-sales_unmatched <- rename(sales_unmatched, CUSTOMER_NAME = CUSTOMER_NAME_y)
+for (i in 1:10) {
+  
+  ### find those records that were not matched and try again (203 records)
+  sales_unmatched <- subset(sales, !(CUSTOMER_NAME_y %in% acc_sales$CUSTOMER_NAME.y ))
+  account_unmatched <- subset(account, !(CUSTOMER_NAME_x %in% acc_sales$CUSTOMER_NAME.x))
+  
+  account_unmatched <- rename(account_unmatched, CUSTOMER_NAME = CUSTOMER_NAME_x)
+  sales_unmatched <- rename(sales_unmatched, CUSTOMER_NAME = CUSTOMER_NAME_y)
+  
+  ### Second JOIN using fuzzyjoin package 
+  x <- stringdist_left_join(account_unmatched, sales_unmatched, by="CUSTOMER_NAME", max_dist = i)
+  acc_sales <- rbind(acc_sales, x %>% filter(CUSTOMER_NAME.y !='NA'))
 
-### Second JOIN using fuzzyjoin package 
-x <- stringdist_left_join(account_unmatched, sales_unmatched, by="CUSTOMER_NAME", max_dist = 1)
-acc_sales <- rbind(acc_sales, x %>% filter(CUSTOMER_NAME.y !='NA'))
+  check <- (nrow(account) - nrow(acc_sales))
+  if (check < 1 ) break;
+} 
 
-
-### find those records that were not matched and try again (60 records)
-sales_unmatched <- subset(sales, !(CUSTOMER_NAME_y %in% acc_sales$CUSTOMER_NAME.y ))
-account_unmatched <- subset(account, !(CUSTOMER_NAME_x %in% acc_sales$CUSTOMER_NAME.x))
-
-account_unmatched <- rename(account_unmatched, CUSTOMER_NAME = CUSTOMER_NAME_x)
-sales_unmatched <- rename(sales_unmatched, CUSTOMER_NAME = CUSTOMER_NAME_y)
-
-x <- stringdist_left_join(account_unmatched, sales_unmatched, by="CUSTOMER_NAME", max_dist = 2)
-acc_sales <- rbind(acc_sales, x %>% filter(CUSTOMER_NAME.y !='NA'))
-
-### find those records that were not matched and try again (17 records)
-sales_unmatched <- subset(sales, !(CUSTOMER_NAME_y %in% acc_sales$CUSTOMER_NAME.y ))
-account_unmatched <- subset(account, !(CUSTOMER_NAME_x %in% acc_sales$CUSTOMER_NAME.x))
-
-account_unmatched <- rename(account_unmatched, CUSTOMER_NAME = CUSTOMER_NAME_x)
-sales_unmatched <- rename(sales_unmatched, CUSTOMER_NAME = CUSTOMER_NAME_y)
-
-x <- stringdist_left_join(account_unmatched, sales_unmatched, by="CUSTOMER_NAME", max_dist = 3)
-acc_sales <- rbind(acc_sales, x %>% filter(CUSTOMER_NAME.y !='NA'))
-
-### find those records that were not matched and try again (11 records)
-sales_unmatched <- subset(sales, !(CUSTOMER_NAME_y %in% acc_sales$CUSTOMER_NAME.y ))
-account_unmatched <- subset(account, !(CUSTOMER_NAME_x %in% acc_sales$CUSTOMER_NAME.x))
-
-account_unmatched <- rename(account_unmatched, CUSTOMER_NAME = CUSTOMER_NAME_x)
-sales_unmatched <- rename(sales_unmatched, CUSTOMER_NAME = CUSTOMER_NAME_y)
-
-x <- stringdist_left_join(account_unmatched, sales_unmatched, by="CUSTOMER_NAME", max_dist = 4)
-acc_sales <- rbind(acc_sales, x %>% filter(CUSTOMER_NAME.y !='NA'))
-
-### find those records that were not matched and try again (6 records)
-sales_unmatched <- subset(sales, !(CUSTOMER_NAME_y %in% acc_sales$CUSTOMER_NAME.y ))
-account_unmatched <- subset(account, !(CUSTOMER_NAME_x %in% acc_sales$CUSTOMER_NAME.x))
-
-account_unmatched <- rename(account_unmatched, CUSTOMER_NAME = CUSTOMER_NAME_x)
-sales_unmatched <- rename(sales_unmatched, CUSTOMER_NAME = CUSTOMER_NAME_y)
-
-x <- stringdist_left_join(account_unmatched, sales_unmatched, by="CUSTOMER_NAME", max_dist = 5)
-acc_sales <- rbind(acc_sales, x %>% filter(CUSTOMER_NAME.y !='NA'))
-
-### find those records that were not matched and try again (2 records)
-sales_unmatched <- subset(sales, !(CUSTOMER_NAME_y %in% acc_sales$CUSTOMER_NAME.y ))
-account_unmatched <- subset(account, !(CUSTOMER_NAME_x %in% acc_sales$CUSTOMER_NAME.x))
-
-account_unmatched <- rename(account_unmatched, CUSTOMER_NAME = CUSTOMER_NAME_x)
-sales_unmatched <- rename(sales_unmatched, CUSTOMER_NAME = CUSTOMER_NAME_y)
-
-x <- stringdist_left_join(account_unmatched, sales_unmatched, by="CUSTOMER_NAME", max_dist = 6)
-acc_sales <- rbind(acc_sales, x %>% filter(CUSTOMER_NAME.y !='NA'))
